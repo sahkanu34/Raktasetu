685ea09fdad0b6486cc5aa285a02c6f38f05243d
sahkanu34 <surajbca111@Nationalinfotechcollege.edu.np>
2025-01-06 19:32:42 +0545

    new commits

--- bloodbanksystem/settings.py
+++ bloodbanksystem/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
         'NAME': 'bbdmspythondb',
         'USER': 'newuser',
         'PASSWORD': '1234',
-        'HOST': 'db',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
 
     }
